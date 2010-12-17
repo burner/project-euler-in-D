b797ac43b5ffef58f2bb0035b9e69cc80e6e0cab
Robert BuRnEr Schadek <rburners@gmail.com>
2010-12-17 22:54:57 +0100

    runall has clean

--- runall.sh
+++ runall.sh
@@ -1,7 +1,27 @@
-#!/bin/bash
+#!/bin/sh
+
+Dirlist=$(find . -maxdepth 1 -type d -name p\*)
+
+if [ "$1" == "clean" ]; 
+then
+	for direc in $Dirlist; 
+	do
+		cd $direc
+		files=$(ls)
+		for obj in $files;
+		do
+			if [[ $obj == *.d ]];
+			then 
+				continue
+			fi	
+			rm $obj
+		done
+		cd ..
+	done
+	exit
+fi
 
 compiler=dmd
-Dirlist=$(find . -maxdepth 1 -type d -name p\*)
 for direc in $Dirlist; 
 do
 	cd $direc
